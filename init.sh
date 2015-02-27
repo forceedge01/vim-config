fcc69d63a9d49aa1fec42c03aeff516a65b4c381
abdul <abdul@rebirth.(none)>
2015-02-27 11:02:48 -0500

    Added init file for setup

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo 'Setting up vimrc for you...'
+# install vundle
+echo 'Cloing vundle package manager for vim...'
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if [[ -f "~/.vimrc" ]]; then
+    echo '~/.vimrc found on machine, replace existing? (The existing one will be moved to ~/.vimrc_old) [y/n]'
+    read userInput
+
+    if [[ $userInput == 'y' ]]; then
+        mv ~/.vimrc ~/.vimrc_old
+    fi
+else
+    touch ~/.vimrc
+fi
+cat ./.vimrc_export >> ~/.vimrc
+vim +PluginInstall +qall
+
+echo 'Setting up bashrc for you...'
+cat ./.bashrc_export >> ~/.bashrc
